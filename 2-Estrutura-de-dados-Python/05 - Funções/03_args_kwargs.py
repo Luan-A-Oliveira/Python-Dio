64a2db1866a95b1487d0d996dc8a37a28c665183
Luan-A-Oliveira <luan.aq.ol@gmail.com>
2023-06-10 12:34:23 -0300

    add tuplas conjuntos dicionarios e funções

--- 2-Estrutura-de-dados-Python/05 - Funções/03_args_kwargs.py
+++ 2-Estrutura-de-dados-Python/05 - Funções/03_args_kwargs.py
@@ -6,6 +6,7 @@ def exibir_poema(data_extenso, *args, **kwargs):
 
 
 exibir_poema(
+    "Sabado, 10 de Junho de 2023",
     "Zen of Python",
     "Beautiful is better than ugly.",
     "Explicit is better than implicit.",
